ac5e64c55ecba135e630e825eda56edd341f2cfc
Sabine Loos <sloos@stanford.edu>
2019-08-27 18:25:53 -0700

    changing plotting functions color identification

--- code/run.sh
+++ code/run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-Rscript -e "rmarkdown::render(input = 'DDF_nb.Rmd', 'html_document',output_dir = '../results', clean = TRUE)"
+Rscript -e "rmarkdown::render(input = 'GDIF_nb.Rmd', 'html_document',output_dir = '../docs/GDIF_nb.html', clean = TRUE)"
